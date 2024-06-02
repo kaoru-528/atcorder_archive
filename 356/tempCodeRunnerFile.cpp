6bbc2b0ee8e7ea5d4e120b0cb555e4aa944078e8
kaoru-528 <origami.kaoru@gmail.com>
2024-06-02 11:04:21 +0900

    356

---
+++ 356/tempCodeRunnerFile.cpp
@@ -0,0 +1,90 @@
+/**
+ * author:  Kaoru
+ * created: 01.06.2024 20:49:26
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+
+#define pb push_back
+#define YESNO(bool) if(bool){cout<<"YES"<<"\n";}else{cout<<"NO"<<"\n";}
+#define yesno(bool) if(bool){cout<<"yes"<<"\n";}else{cout<<"no"<<"\n";}
+#define YesNo(bool) if(bool){cout<<"Yes"<<"\n";}else{cout<<"No"<<"\n";}
+
+template <typename T> inline void print(const vector<T>& v, string s = " "){rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p){cout << p.first << " " << p.second << "\n";}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v){for (auto&& p : v) print(p);}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main()
+{
+    int N,M,K;
+    cin >> N >> M >> K;
+    vvi A(M);
+    vector<char> R(M);
+
+    rep(i, M){
+        int C;
+        cin >> C;
+        A[i].resize(C);
+        for (int j = 0; j < C; ++j) {
+            cin >> A[i][j];
+            A[i][j]--;
+        }
+        cin >> R[i];
+    }
+
+    int ans = 0;
+
+    for (int bit = 0; bit < (1 << N); bit++) {
+        // bit は 0 から 2^N-1
+        bool valid = true;
+        for (int i = 0; i < M && valid; i++) {
+            int cnt = 0;
+           for (int j = 0; j < (int)A[i].size(); j++) {
+                int key = A[i][j];
+                // bit の key 番目の bit が立っているかどうか
+                if (bit & (1 << key)) {
+                    cnt++;
+                }
+            }
+
+            if ((R[i] == 'o' && cnt < K) || (R[i] == 'x' && cnt >= K)) {
+                valid = false;
+            }
+        }
+
+        if (valid) {
+            ans++;
+        }
+    }
+    print(ans);
+    return 0;
+}
