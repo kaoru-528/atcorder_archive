8caf10452bac790483fe2007d4b5098f985aeca3
kaoru-528 <origami.kaoru@gmail.com>
2024-09-29 16:12:31 +0900

    373

---
+++ 373/e.cpp
@@ -0,0 +1,79 @@
+/**
+ * author:  Kaoru
+ * created: 28.09.2024 21:51:42
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+
+#define pb push_back
+#define YESNO(bool) if(bool){cout<<"YES"<<"\n";}else{cout<<"NO"<<"\n";}
+#define yesno(bool) if(bool){cout<<"yes"<<"\n";}else{cout<<"no"<<"\n";}
+#define YesNo(bool) if(bool){cout<<"Yes"<<"\n";}else{cout<<"No"<<"\n";}
+
+template <typename T> inline void print(const vector<T>& v, string s = " "){rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p){cout << p.first << " " << p.second << "\n";}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v){for (auto&& p : v) print(p);}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main()
+{
+    ll N, M, K;
+    cin >> N >> M >> K;
+    ll sum = 0;
+    vll A(N), B(N);
+    rep(i, N){
+        cin >> A[i];
+        B[i] = A[i];
+        sum += A[i];
+    }
+    sort(rall(B));
+    ll min = B[M-1];
+    if(min == B[M] || min == B[M-2]){
+        for(int i = M; i >= 0; i--){
+            if(B[i] != min){
+                min = B[i];
+                break;
+            }
+        }
+    }
+    print(min);
+    vll ans(N);
+    rep(i, N){
+        if(min > A[i]+(K-sum)){
+            ans[i] = -1;
+        }
+        else{
+            ans[i] = min - A[i]+1;
+        }
+    }
+    print(ans);
+    return 0;
+}
