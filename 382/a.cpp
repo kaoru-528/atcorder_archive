608639da9fe7b82a6cf17782e9d257837f6beb0c
kaoru-528 <origami.kaoru@gmail.com>
2024-12-01 11:01:45 +0900

    382

---
+++ 382/a.cpp
@@ -0,0 +1,76 @@
+/**
+ * author:  Kaoru
+ * created: 30.11.2024 20:59:16
+ **/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<long long>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<long long, long long>;
+
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
+#define all(...) std::begin(__VA_ARGS__), std::end(__VA_ARGS__)
+#define rall(...) std::rbegin(__VA_ARGS__), std::rend(__VA_ARGS__)
+
+#define pb push_back
+#define YESNO(bool) if(bool){cout<<"YES"<<"\n";}else{cout<<"NO"<<"\n";}
+#define yesno(bool) if(bool){cout<<"yes"<<"\n";}else{cout<<"no"<<"\n";}
+#define YesNo(bool) if(bool){cout<<"Yes"<<"\n";}else{cout<<"No"<<"\n";}
+
+template <typename T> inline void print(const vector<T>& v, string s = " "){rep(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+template <typename T, typename S> inline void print(const pair<T, S>& p){cout << p.first << " " << p.second << "\n";}
+template <typename T> inline void print(const T& x) {cout << x << "\n";}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v){for (auto&& p : v) print(p);}
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+int main()
+{
+    long long N, M;
+    cin >> N >> M;
+    vector<int> A(N), B(M);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    for (int i = 0; i < M; i++) {
+        cin >> B[i];
+    }
+
+    vector<int> min_idx(1000000, N + 1);
+
+    for (int i = 0; i < N; i++) {
+        min_idx[A[i]] = min(min_idx[A[i]], i);
+    }
+
+    for (int i = 0; i < 1000000-1; i++) {
+        min_idx[i + 1] = min(min_idx[i + 1], min_idx[i]);
+    }
+
+    for (int i = 0; i < M; i++) {
+        if (min_idx[B[i]] < N) {
+            cout << min_idx[B[i]] + 1 << endl;
+        } else {
+            cout << -1 << endl;
+        }
+    }
+    return 0;
+}
